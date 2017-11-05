2a3747881e119bea75c92b84dffd9e2dd229a3a4
mattsliv <msliv12275@gmail.com>
2017-11-05 13:25:22 -0500

    updates

--- umasshack/static/js/main.js
+++ umasshack/static/js/main.js
@@ -2,32 +2,42 @@ $(function() {
 	$("input").focus();
 });
 
-// $.get('file_to_read.txt', function(data) {
-//    if ($("#tempRead").val())
-// }, 'text');
+$(document).ready(function() {
+    $("#tempRead").click(function() {
+        $.ajax({
+            url : "file:///Users/msliv/Documents/umasshackv/umasshack/Arduino/temp_display/temp_data.txt",
+            dataType: "text",
+            success : function (data) {
+                $(".text").html(data);
+            }
+        });
+    });
+});
 
 function readFile(file)
 {
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-								"#tempRead" = allText.val();
-                alert(allText);
-            }
-        }
-    }
-    rawFile.send(null);
+    // var rawFile = new XMLHttpRequest();
+    // rawFile.open("GET", file, false);
+    // rawFile.onreadystatechange = function ()
+    // {
+    //     if(rawFile.readyState === 4)
+    //     {
+    //         if(rawFile.status === 200 || rawFile.status == 0)
+    //         {
+    //             var allText = rawFile.responseText;
+		// 						"#tempRead" = allText.val();
+    //             alert(allText);
+    //         }
+    //     }
+    // }
+    // rawFile.send(null);
 
 		// if ($("#tempRead").val() == allText.val())
 		// {
 		// 	$("#tempRead").val() = all;
 		// }
+
+		(".text").load(file);
 }
 
 $(function timeOut() {
